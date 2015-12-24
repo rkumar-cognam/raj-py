a8a4a0cdd24fa3ede3e4ec59c6e3bdc7b5f96af6
rkumar-cognam <rkumar@cognam.com>
2015-12-24 17:12:47 +0530

    Create raj.py

---
+++ raj.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2.7
+
+print 'Hi, This is my first test python program'
+
+name = raw_input ('Please give your name')
+
+print 'Hi, %s.' % name
+print 'This is a  test program only'
